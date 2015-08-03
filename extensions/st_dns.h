53b9512ec16b3aa1888474c9c868f780cf49437f
Oleg Trizna <otrizna@yahoo.com>
2015-08-03 12:53:14 -0700

    add extern "C" for C++ compilations

--- extensions/st_dns.h
+++ extensions/st_dns.h
@@ -2,4 +2,10 @@
 #include <arpa/inet.h>
 #include <netdb.h>
 
+#ifdef __cplusplus
+extern "C" {
+#endif
 extern int st_gethostbyname_r(const char *name, struct hostent **host);
+#ifdef __cplusplus
+}
+#endif
