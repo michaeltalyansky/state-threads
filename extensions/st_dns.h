cd44ea68c13ab2f1b56cfe45b4698b92a4e92a6d
Oleg Trizna <otrizna@yahoo.com>
2015-09-25 17:04:14 -0700

    add prototype for st_gethostbyname2_r

--- extensions/st_dns.h
+++ extensions/st_dns.h
@@ -6,6 +6,7 @@
 extern "C" {
 #endif
 extern int st_gethostbyname_r(const char *name, struct hostent **host);
+extern int st_gethostbyname2_r(const char *name, int family, struct hostent **host);
 #ifdef __cplusplus
 }
 #endif
